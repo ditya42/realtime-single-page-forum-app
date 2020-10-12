d87f698a72f4f66e9497b58e2678184eddb81d79
aditya rahman <aditya.rahman41@gmail.com>
2020-10-12 15:18:17 +0800

    section 9 about JWT Authentication

--- app/User.php
+++ app/User.php
@@ -5,8 +5,9 @@ namespace App;
 use App\Model\Question;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -32,4 +33,20 @@ class User extends Authenticatable
         return $this->hasMany(Question::class);
 
     }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
 }
